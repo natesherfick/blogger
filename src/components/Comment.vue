f10104d376a055fd66cabdb975aaffb34702d12a
Nate Sherfick <natesherfick@gmail.com>
2020-04-20 19:42:19 -0600

    blog details and comments displaying

---
+++ src/components/Comment.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="Comment col-7 my-1">
+    <div class="card p-4">
+<p class=""><b>{{commentData.body}}</b></p>
+<p>-{{commentData.creator.name}}</p>
+</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Comment',
+  props: ["commentData"],
+  data(){
+    return {}
+  },
+  mounted:{}, 
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- src/pages/BlogDetails.vue
+++ src/pages/BlogDetails.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="blog-details">
-blog deets
-
+  <div class="blog-details container-fluid">
+    <div class="row d-flex justify-content-center">
+<div class="card col-7 my-3">
+  <img class="card-img-top img-fluid" :src="activeBlog.blog.imgUrl" alt="Card image cap">
+  <div class="card-body">
+    <h6>{{activeBlog.blog.title}}: posted by {{activeBlog.blog.creator.name}}</h6>
+    <p class="card-text">{{activeBlog.blog.body}}</p>
+  </div>
+</div>
+</div>
+<div class="row d-flex justify-content-center">
+<h3 class="col-7">Comments</h3>
+<Comment v-for="comment in activeComments" :commentData="comment" :key="comment._id"></Comment>
+</div>
   </div>
 </template>
 
 
 <script>
+  import Comment from "../components/Comment.vue"
 export default {
   name: 'blog-details',
   data(){
@@ -15,9 +27,20 @@ export default {
   mounted(){
     this.$store.dispatch("getBlog", this.$route.params.blogId)
   }, 
-  computed:{},
+  computed:{
+    activeBlog() {
+      return this.$store.state.activeBlog
+      console.log(this.$store.state.activeBlog);
+      
+    },
+    activeComments() {
+      return this.$store.state.activeBlog.comments
+    },
+  },
   methods:{},
-  components:{}
+  components:{
+    Comment
+  }
 }
 </script>
 
